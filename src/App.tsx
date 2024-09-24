a9d1da654b50a8b7ea86513eb51dae5b668209ab
taehongkim <public.bluemiv@gmail.com>
2024-09-24 20:32:02 +0900

    feat: tailwindcss 공통 스타일 설정 #6

--- src/App.tsx
+++
@@ -1,9 +0,0 @@
-import { RouterProvider } from 'react-router-dom';
-
-import { router } from '@/features/router';
-
-function App() {
-  return <RouterProvider router={router}></RouterProvider>;
-}
-
-export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+const colors = {
+  primary: {
+    DEFAULT: '#1677FF',
+    50: '#e6f7ff',
+    100: '#bae7ff',
+    200: '#91d5ff',
+    300: '#69c0ff',
+    400: '#40a9ff',
+    500: '#1890ff',
+    600: '#096dd9',
+    700: '#0050b3',
+    800: '#003a8c',
+    900: '#002766',
+  },
+};
+
+const spacing = {
+  xxs: '0.25rem',
+  xs: '0.5rem',
+  sm: '0.75rem',
+  md: '1rem',
+  lg: '1.5rem',
+  xl: '2rem',
+  xxl: '3rem',
+};
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors,
+      spacing,
+      margin: spacing,
+      padding: spacing,
+      gap: spacing,
+    },
+  },
+  plugins: [],
+};
